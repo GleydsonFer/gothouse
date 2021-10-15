2900bad35a9c9c61a7561a5ce1ee3507d0e4af71
Gleydson Ferreira Coelho <gleydson.coelho@adagri.local>
2021-10-15 14:05:28 -0300

    criando casas

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h3>Teste</h3>
+      <h3>Stark</h3>
+      <h3>Baratheon</h3>
+      <h3>Targaryen</h3>
+      <h3>Greyjoy</h3>
+      <h3>Lannister</h3>
     </div>
   );
 }
